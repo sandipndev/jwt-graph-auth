2eb045343ff381f07955961579a0897f1167bce2
Sandipan Dey <sandipandey.12@gmail.com>
2020-05-31 19:16:38 +0530

    Added apollo with gql. Fixed nodemon ts refresh.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hi");
+import mongoose from "mongoose";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+import { MONGODB_URL, APP_PORT } from "./config";
+
+(async () => {
+  await mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    typeDefs: gql`
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "hi",
+      },
+    },
+  });
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(APP_PORT, () => console.log("🚀 Apollo Server - Port", APP_PORT));
+})();
