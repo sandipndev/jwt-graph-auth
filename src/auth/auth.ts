6584f3fc06a617fd6a842e3b65aaa54cafdf954e
Sandipan Dey <sandipandey.12@gmail.com>
2020-06-02 09:00:49 +0530

    tokens getting cross checked now

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -16,8 +16,14 @@ export const isAuth: MiddlewareFn<apolloCtx> = async ({ context }, next) => {
   try {
     const token = authorization.split(" ")[1];
     const payload = verify(token, SECRET_ACCESSTOKEN) as tokenPayload;
+
     const user = await User.findById(payload.userId);
     if (!user) throw new AuthenticationError("Unauthorized user");
+
+    const userWhitelist = user.whitelistedAccessTokens;
+    if (!userWhitelist.includes(token))
+      throw new AuthenticationError("Bad token");
+
     context.user = user;
   } catch {
     throw new AuthenticationError("Bad token");
